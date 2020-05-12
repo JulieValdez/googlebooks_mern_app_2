a4ad4ab0096259132a08e734039ad8b4a9b26055
JulieValdez <juliegragg0605@gmail.com>
2020-05-12 16:54:22 -0700

    fixed react running again

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+  return (
+    <div className="jumbotron text-center">
+      <h1>Google Books Search</h1>
+      <h4>Search for and Save Books from the Google Books Library</h4>
+    </div>
+  );
+}
+
+export default Jumbotron;
